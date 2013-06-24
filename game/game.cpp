351ba1278457da682edcfa22d2a8f286db3fefd0
Steven Downum <devilaether@gmail.com>
2013-06-23 22:07:06 -0500

    Finished Fritzing diagram for project. Verbage changes in documentation and comments.

--- game/game.cpp
+++ game/game.cpp
@@ -11,7 +11,7 @@ A game of timing with an array of LEDs and a switch.
 This goal is to press the switch when the green LED is lit. The more times
 you succeed, the faster the LEDs flash.
 
-Besd on sketches for Chapter 4 from the OSEPP "Arduino Companion" book,
+Besd on sketches for Tutorial 4 from the OSEPP "Arduino Companion" book,
 *Arduino Basics*.
 
 
